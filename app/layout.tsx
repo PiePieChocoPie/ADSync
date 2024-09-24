85a7c130c3d4a1dbf659b8980ea7835931ea67e2
Alexandr <39surara@gmail.com>
2024-09-24 16:35:23 +0500

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,14 @@
+
+import '@/app/ui/global.css';
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
